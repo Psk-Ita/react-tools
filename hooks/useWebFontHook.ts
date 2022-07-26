d22aca16ae325bbf98982da9aa59abdee92a1b24
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2022-07-26 16:38:14 +0200

    Update useWebFontHook.ts

--- hooks/useWebFontHook.ts
+++ hooks/useWebFontHook.ts
@@ -1,60 +1,80 @@
-export interface WebFontProps {
-  baseUrl: string;          // base font url : relative, absolute, external
-  fontName: String;         // file name used to load font resource
-  fontFamily: String;       // css name reference
-  isDefault?: boolean;      // used as font-family for html and body 
-  ignoreStyles?: boolean;   // specify to load or not font variations
+import { Dictionary } from 'lodash';
+
+export enum FontStyles {
+  normal = 'normal',
+  italic = 'italic'
 }
 
-export const useWebFont = ({ baseUrl, fontName, fontFamily, isDefault, ignoreStyles }: WebFontProps): void => {
-  const fontDefinition = ignoreStyles ? `
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily}"), local("${fontName}"), url("${baseUrl}/${fontName}.woff2") format("woff2"), url("${baseUrl}/${fontName}.woff") format("woff"); font-weight: normal; font-style: normal; }
-`:`
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Bold"), local("${fontName}-Bold"), url("${baseUrl}/${fontName}-Bold.woff2") format("woff2"), url("${baseUrl}/${fontName}-Bold.woff") format("woff"); font-weight: bold; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Black"), local("${fontName}-Black"), url("${baseUrl}/${fontName}-Black.woff2") format("woff2"), url("${baseUrl}/${fontName}-Black.woff") format("woff"); font-weight: 900; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Light"), local("${fontName}-Light"), url("${baseUrl}/${fontName}-Light.woff2") format("woff2"), url("${baseUrl}/${fontName}-Light.woff") format("woff"); font-weight: 300; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Italic"), local("${fontName}-Italic"), url("${baseUrl}/${fontName}-Italic.woff2") format("woff2"), url("${baseUrl}/${fontName}-Italic.woff") format("woff"); font-weight: normal; font-style: italic; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Regular"), local("${fontName}-Regular"), url("${baseUrl}/${fontName}-Regular.woff2") format("woff2"), url("${baseUrl}/${fontName}-Regular.woff") format("woff"); font-weight: normal; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} SemiBold"), local("${fontName}-SemiBold"), url("${baseUrl}/${fontName}-SemiBold.woff2") format("woff2"), url("${baseUrl}/${fontName}-SemiBold.woff") format("woff"); font-weight: 600; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} ExtraLight"), local("${fontName}-ExtraLight"), url("${baseUrl}/${fontName}-ExtraLight.woff2") format("woff2"), url("${baseUrl}/${fontName}-ExtraLight.woff") format("woff"); font-weight: 200; font-style: normal; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Bold Italic"), local("${fontName}-BoldItalic"), url("${baseUrl}/${fontName}-BoldItalic.woff2") format("woff2"), url("${baseUrl}/${fontName}-BoldItalic.woff") format("woff"); font-weight: bold; font-style: italic; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} Light Italic"), local("${fontName}-LightItalic"), url("${baseUrl}/${fontName}-LightItalic.woff2") format("woff2"), url("${baseUrl}/${fontName}-LightItalic.woff") format("woff"); font-weight: 300; font-style: italic; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} SemiBold Italic"), local("${fontName}-SemiBoldItalic"), url("${baseUrl}/${fontName}-SemiBoldItalic.woff2") format("woff2"), url("${baseUrl}/${fontName}-SemiBoldItalic.woff") format("woff"); font-weight: 600; font-style: italic; }
-@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} ExtraLight Italic"), local("${fontName}-ExtraLightItalic"), url("${baseUrl}/${fontName}-ExtraLightItalic.woff2") format("woff2"), url("${baseUrl}/${fontName}-ExtraLightItalic.woff") format("woff"); font-weight: 200; font-style: italic; }
-`;
+export enum FontWeigths {
+  Bold = 700,
+  Regular = 400,
+  SemiBold = 600
+}
 
-  const cssId = `EFC-${fontName}-Font`;
-  let domCss = document.getElementById(cssId);
-  if (!domCss) {
-    domCss = document.createElement("style");
-    domCss.id = cssId;
-    document.head.appendChild(domCss);
-  }
-  domCss.innerHTML = fontDefinition;
+export interface WebFontProps {
+  baseUrl: string;
+  fontName: String;
+  fontFamily: String;
+  isDefault?: boolean;
+  weights?: FontWeigths[] | undefined;
+  style?: FontStyles | undefined;
+}
 
-  if (isDefault) {
-    ["woff2", "woff"].forEach((format: string) => {
-      const linkId = `EFC-${fontName}-${format}-Preload`;
+export const useWebFont = (props: WebFontProps[]): void => {
+  props.forEach(({ baseUrl, fontName, fontFamily, isDefault, weights, style }) => {
+    const uid = `${fontName}-${(weights || []).join('-')}${style ? `-${style}` : ''}`;
 
-      let domLink = document.getElementById(linkId);
-      if (!domLink) {
-        domLink = document.createElement("link");
-        domLink.id = linkId;
-        document.head.appendChild(domLink);
+    let fontStyleIn = `${style || ''}`;
+    let fontStyleOut = `${style || FontStyles.normal}`;
+
+    let fontDefinition: Dictionary<string> = {};
+    if (Array.isArray(weights) && weights.length > 0) {
+      weights.forEach(x => {
+        const weigth = FontWeigths[x];
+        fontDefinition[weigth] = `@font-face { font-family: "${fontFamily}"; src: local("${fontFamily} ${weigth}${
+          style ? ` ${style}` : ''
+        }"), local("${fontName}-${x}${fontStyleIn}"), url("${baseUrl}/${fontName}-${x}${fontStyleIn}.woff2") format("woff2"), url("${baseUrl}/${fontName}-${x}${fontStyleIn}.woff") format("woff"); font-weight: ${weigth}; font-style: ${fontStyleOut}; }`;
+      });
+    } else {
+      fontDefinition[
+        FontWeigths[FontWeigths.Regular]
+      ] = `@font-face { font-family: "${fontFamily}"; src: local("${fontFamily}"), local("${fontName}${fontStyleIn}"), url("${baseUrl}/${fontName}${fontStyleIn}.woff2") format("woff2"), url("${baseUrl}/${fontName}${fontStyleIn}.woff") format("woff"); font-weight: normal; font-style: ${fontStyleOut}; }`;
+    }
+
+    const cssId = `${isDefault ? 'DEF' : 'EFC'}-${uid}-Font`;
+    let domCss = document.getElementById(cssId);
+    if (!domCss) {
+      domCss = document.createElement('style');
+      domCss.id = cssId;
+      document.head.appendChild(domCss);
+      let out = '';
+      for (const k in fontDefinition) {
+        out += `
+      ${fontDefinition[k]}`;
       }
+      domCss.innerHTML = out;
+    }
 
-      if (domLink instanceof HTMLLinkElement) {
-        domLink["as"] = "style";
-        domLink["rel"] = "preload";
-        domLink["href"] = `${baseUrl}/${fontName}${ignoreStyles ? '':'-Regular'}.${format}`;
-      }
-    });
+    if (isDefault) {
+      ['woff', 'woff2'].forEach((format: string) => {
+        const linkId = `EFC-${uid}-${format}-Preload`;
 
-    domCss.append(`
+        let domLink = document.getElementById(linkId);
+        if (!domLink) {
+          domLink = document.createElement('link');
+          domLink.id = linkId;
+          document.head.appendChild(domLink);
+
+          if (domLink instanceof HTMLLinkElement) {
+            domLink['as'] = 'style';
+            domLink['rel'] = 'preload';
+            domLink['href'] = `${baseUrl}/${fontName}-${FontWeigths.Regular}.${format}`;
+          }
+        }
+      });
+
+      domCss.append(`
 html, body { font-family: '${fontFamily}'; font-style: normal; }`);
-  }
+    }
+  });
 };
-
-/* usage
-  useWebFont({ fontFamily: "Sansation", fontName: "0230a71f60ccd179297f937585f315e4", baseUrl: "//db.onlinewebfonts.com/t", isDefault: true, ignoreStyles: true });
-*/
